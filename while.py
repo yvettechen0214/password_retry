e7c7a60276ee45095d7d920e19666e93f1225c0c
yvette chen <annd840214@gmail.com>
2021-10-05 21:32:28 +0800

    update version

--- while.py
+++ while.py
@@ -1,6 +1,6 @@
 password = "a123456"
 n = 3
-while True:
+while n > 0:
     user_password = input("請輸入密碼：")
     if user_password == password:
         print("登入成功！")
@@ -8,5 +8,4 @@ while True:
     else:
         n = n - 1 
         print("密碼錯誤！還有", n,"次機會")
-        if n == 0:
-            break
+ 
