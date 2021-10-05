b628c43b889594a9f74074aca23a7da3179337bd
yvette chen <annd840214@gmail.com>
2021-10-05 21:44:40 +0800

    final version

--- while.py
+++ while.py
@@ -1,11 +1,14 @@
 password = "a123456"
 n = 3
 while n > 0:
+    n = n - 1
     user_password = input("請輸入密碼：")
     if user_password == password:
         print("登入成功！")
         break
     else:
-        n = n - 1 
-        print("密碼錯誤！還有", n,"次機會")
- 
+        print("密碼錯誤！")
+        if n > 0:
+            print("還有", n,"次機會")
+        else:
+            print("沒機會了，要鎖帳號了！")
